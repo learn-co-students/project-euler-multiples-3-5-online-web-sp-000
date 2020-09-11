f4c43e3e2fb62fdb90f3d7d71f815ad069ca09e5
Jane Venter <janeanneventer@gmail.com>
2020-09-11 20:34:07 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,24 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+   array = []
+   i = 1
+   until i == limit do
+      times3 = 3*i 
+      times5 = 5*i
+      if times3 < limit && !array.include?(times3)
+        array.push(times3)
+        # puts times3
+      end
+      if times5 < limit
+        array.push(times5)
+      end
+     i+=1
+   end
+   #array.reduce(0){ |i, t| i + t }
+   array.sort()
+end
+
+def sum_multiples(limit)
+  array = collect_multiples(limit)
+  array.reduce(0){|i, t| i + t }
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,30 @@
 # Enter your object-oriented solution here!
+class Multiples
+  attr_accessor :limit
+  def initialize(limit)
+    @limit = limit
+  end
+  
+  def collect_multiples
+    array = []
+    i = 1 
+    until i == self.limit
+      times3 = i*3 
+      times5 = i*5 
+      if times3 < self.limit && !array.include?(times3)
+        array.push(times3)
+      end
+      if times5 < self.limit 
+        array.push(times5)
+      end
+      i+=1
+    end
+    array.sort()
+  end
+  
+  def sum_multiples
+    array = self.collect_multiples 
+    array.reduce(0){|item, total| item+ total}
+  end
+  
+end
