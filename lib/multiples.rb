48e64cbf8a766f1ac0aff03247f9c1fb07b45ecd
boom100100 <bsd231@nyu.edu>
2020-09-17 11:43:06 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,15 @@
 # Enter your procedural solution here!
+
+def collect_multiples(limit)
+  target = []
+  i = 1
+  loop do
+    target << (i) if (i % 3 == 0) || (i % 5 == 0)
+    break if (i += 1) == limit
+  end
+  target
+end
+
+def sum_multiples(limit)
+  collect_multiples(limit).sum
+end
