507c6471066bdb3a6502e758002702b6bd68f9a1
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-12-10 17:04:14 +0100

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,18 @@
 # Enter your procedural solution here!
+def collect_multiples(num)
+    result = []
+    counter = 1
+
+    until counter === num
+        if counter % 3 === 0 || counter % 5 === 0
+            result << counter
+        end
+        counter += 1
+    end
+
+    result
+end
+
+def sum_multiples(num)
+    (collect_multiples(num)).reduce(:+)
+end
