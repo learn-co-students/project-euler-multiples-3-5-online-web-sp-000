03df6c9f01cd455fd69d4d00395abfc834bed433
blumenthaler <blumenthaleralex@gmail.com>
2021-01-28 11:34:51 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,17 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+    result = []
+    counter = limit - 1
+    until counter === 0
+        if counter % 3 === 0 || counter % 5 === 0
+            result.unshift(counter)
+        end
+        counter -= 1
+    end
+    result
+end
+
+def sum_multiples(limit)
+    array = collect_multiples(limit)
+    array.reduce(:+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,24 @@
 # Enter your object-oriented solution here!
+class Multiples
+    attr_accessor :limit
+
+    def initialize(limit)
+        self.limit = limit
+    end
+
+    def collect_multiples
+        result = []
+        counter = self.limit - 1
+        until counter === 0
+            if counter % 3 === 0 || counter % 5 === 0
+                result.unshift(counter)
+            end
+            counter -= 1
+        end
+        result
+    end
+
+    def sum_multiples
+        collect_multiples.reduce(:+)
+    end
+end
