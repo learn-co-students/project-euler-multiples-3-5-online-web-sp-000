90d132003c9f52cbfd5da9fa50d024aab9a95681
Alexander M. Clamor <alexander.clamor@gmail.com>
2021-05-03 16:07:32 -0700

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,29 @@
-# Enter your object-oriented solution here!
+class Multiples
+
+    def initialize(limit)
+        @limit = limit
+    end
+
+    def collect_multiples
+        arr = [3, 5]
+        result = []
+    
+        for num in arr    
+            int = 1
+            while (num * int) < @limit
+                result.push(num * int)
+                int += 1
+            end
+        end
+        result.uniq.sort
+    end
+
+    def sum_multiples
+        collect_multiples.sum
+    end
+
+end
+
+# multiples = Multiples.new(10)
+# puts multiples.collect_multiples.inspect
+# puts multiples.sum_multiples.inspect
